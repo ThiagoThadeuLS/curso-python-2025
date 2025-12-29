6aca7a8c40595b656e53fd2064b55132a0b420cc
Thiago Thadeu <thiago.thadeu11@gmail.com>
2025-12-29 00:33:57 -0300

    aula módulos

---
+++ dia_06/07_modulos_lib.py
@@ -0,0 +1,24 @@
+# %%
+import math
+from math import pi, e
+from math import * # evitar MUITO usar
+
+import math as mh
+
+# %%
+math.sqrt(9)
+
+# %%
+math.pow(2, 4)
+
+# %%
+math.e
+
+# %%
+math.pi
+
+# %%
+pi
+
+# %%
+e
---
+++ dia_06/08_aleatorio.py
@@ -0,0 +1,11 @@
+# %%
+
+import random
+# %%
+random.randint(1, 10)
+
+# %%
+
+x = ['Thadeu', 'Duda', 'Dado', 'Filipe']
+
+random.choice(x)
